0e7108c5b8b1616822333edee54af23e209fbe3c
AKASH007676 <158271327+AKASH007676@users.noreply.github.com>
2025-01-08 00:51:19 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,13 +5,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348165846414"
+global.ownernomer = "917872324809"
 global.ownername = "𝑻𝒐𝒙𝒙𝒊𝒄 𝑩𝒐𝒚"
 global.ytname = "YT: ToxxicTech"
 global.socialm = "GitHub: Toxic1239"
 global.location = "Nigeria"
 
-global.ownernumber = '2348165846414'  //creator number
+global.ownernumber = '917872324809'  //creator number
 global.ownername = '𝑻𝒐𝒙𝒙𝒊𝒄 𝑻𝒆𝒄𝒉' //owner name
 global.botname = '𝒁𝒆𝒏𝒐𝒏 𝒁𝒐𝒈𝒓𝒂𝒕𝒊𝒔' //name of the bot
 
@@ -23,7 +23,7 @@ ANDROID CAN USE ANY VERSION
 V1 === NO BUTTON
 V2 === BUTTON MODE */
 
-global.typemenu = 'v1' 
+global.typemenu = 'v2' 
 
 
 
